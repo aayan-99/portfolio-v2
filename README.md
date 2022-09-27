0d21a3d370bf032bfaa65b2ec43e9d47700fb34f
unknown <aayan.work99@gmail.com>
2022-09-27 21:50:00 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# PORTFOLIO
+
+This is my portfolio web application showing all my skills and projects.
+
+## [Demo Link](https://portfolio-v1-teal.vercel.app) 🔗
+
+
+## Technologies used 🛠️
+
+- **HTML** 🚀
+- **CSS** 🚀
+- **JavaScript** 🚀
+- **Tailwind CSS** 🚀
+- **React JS** 🚀
+
+
+
+## How To Use 🔧
+
+From your command line, first clone this repo:
+
+```bash
+
+# Clone this repository
+$ git clone https://github.com/aayan-99/portfolio-v1-teal.vercel.app
+
+# Go into the repository
+$ cd portfolio_v1
+
+# Remove current origin repository
+$ git remote remove origin
+
+```
+
+Then you can install the dependencies
+
+Using NPM:
+
+```bash
+
+# Install dependencies
+$ npm install
+
+```
+
+## Author 👨‍💻
+
+- **Aayanjit Bhuyan** - **[Linkedin](https://www.linkedin.com/in/aayanjit-bhuyan-b48705195/)**, **[Github](https://github.com/aayan-99)**, **[Portfolio](https://portfolio-v1-teal.vercel.app/)**
+
+<br>
+
+#### Note:
+
+I'm currently looking for good **Job Opportunities** both **Remote ( Worldwide )** and **On-Site ( Anywhere in India )**. So, if you have a good opportunity that matches my skills experience then you can contact me on my **[Linkedin](https://www.linkedin.com/in/aayanjit-bhuyan-b48705195/)** or my email id **aayan.work99@gmail.com** 🙌
+
+<br>
+
+---
---
+++ src/Components/Work.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Netflix from '../assets/images/netflix.png'
+import Crypto from '../assets/images/crypto.jpg'
+
+const Work = () => {
+    return (
+        <div name="work" className='w-full sm:h-screen text-gray-300 bg-[#0a192f]'>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Work</p>
+                    <p className='py-6'>// Check out some of my recent work.</p>
+                </div>
+
+                {/* Container */}
+                <div className='grid sm:grid-cols-2 gap-4'>
+
+                    {/* Grid Item */}
+                    <div style={{ backgroundImage: `url(${Netflix})` }} className='shadow-lg shadow-[040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100 p-4'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                Netflix clone using React Js and Firebase.
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="https://netflix-clone-aayan-99.vercel.app/" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Demo</button></a>
+                                <a href="https://github.com/aayan-99/Netflix-Clone" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Code</button></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Grid Item */}
+                    <div style={{ backgroundImage: `url(${Crypto})` }} className='shadow-lg shadow-[040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+
+                        {/* Hover Effects */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+                                ReactJs Application
+                            </span>
+                            <div className='pt-8 text-center'>
+                                <a href="https://alpha-oryx.vercel.app/" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Demo</button></a>
+                                <a href="https://github.com/aayan-99/alpha-oryx" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Code</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Work
