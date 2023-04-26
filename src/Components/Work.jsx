ab2acc130c46f1e40a2c97a8dcc422fe160b1721
Aayan-ekoinfomatics <aayanjit@ekoinfomatics.com>
2023-04-26 18:28:43 +0530

    links updated in work

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -55,8 +55,8 @@ const Work = () => {
                                 Air Bnb Clone using NEXT.Js
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href="https://nukemusix.vercel.app/" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Demo</button></a>
-                                <a href="https://github.com/aayan-99/nukemusix" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Code</button></a>
+                                <a href="air-bnb-clone-aayan-99.vercel.app/" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Demo</button></a>
+                                <a href="https://github.com/aayan-99/air-bnb-clone" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Code</button></a>
                             </div>
                         </div>
                     </div>
@@ -70,8 +70,8 @@ const Work = () => {
                                 Crypto tracking website using ReactJs
                             </span>
                             <div className='pt-8 text-center'>
-                                <a href="https://nukemusix.vercel.app/" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Demo</button></a>
-                                <a href="https://github.com/aayan-99/nukemusix" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Code</button></a>
+                                <a href="alpha-oryx.vercel.app" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Demo</button></a>
+                                <a href="https://github.com/aayan-99/alpha-oryx" target='_blank' rel='noreferrer'><button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold tect-lg'>Code</button></a>
                             </div>
                         </div>
                     </div>
